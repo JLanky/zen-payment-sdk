44b8ba3b21bb32e88a0087b8d696af61001841b3
JLanky <lanky@i.ua>
2024-12-24 21:08:33 +0200

    Refactoring

--- tests/Functional/Purchase/CreateTransaction/CreateTransactionTest.php
+++ tests/Functional/Purchase/CreateTransaction/CreateTransactionTest.php
@@ -38,6 +38,7 @@ class CreateTransactionTest extends ZenFunctionalTestCase
         $this->assertSame(ResponseBodyEnum::StatusAccepted->value, $responseData->getStatus());
         $this->assertSame(ResponseBodyEnum::TypeTrtRefund->value, $responseData->getType());
         $this->assertSame(ResponseBodyEnum::PaymentChannel->value, $responseData->getPaymentChannel());
+        $this->assertSame(ResponseBodyEnum::CustomerEmail->value, $responseData->getPaymentChannel());
     }
 
     public static function getTestData(): array
